6ff99668fe3ac5b218e985a67b33936a8f96e725
nikesh deula <nikeshdeula.21@gmail.com>
2024-02-16 07:57:57 +0545

    add_to_cart functionality is added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\cart;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
@@ -17,4 +19,24 @@ class ProductController extends Controller
       return view('detail', ['product' => $data]);
         
     }
+
+    public function addToCart(Request $req){
+      if($req->session()->has('user')){
+        // echo 'add to cart';
+        $cart = new cart;
+        $cart->user_id=$req->session()->get('user')['id'];
+        $cart->product_id=$req->product_id;
+        $cart->save();
+        return redirect('/product');
+
+      }else {
+        return redirect('login');
+      }
+    }
+
+    static function cartItem(){
+      $userId = Session::get('user')['id'];
+      return cart::where('user_id', $userId)->count();
+    }
 }
+
